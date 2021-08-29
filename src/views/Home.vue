fc81a0021b85b75403ea24fb24da4879f2630f21
tim1047 <tim1047@naevr.com>
2021-08-29 17:02:03 +0900

    axios 연동 테스트

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <input type="text" v-model="name"/>
-    <span>{{name}}</span>
     <button v-on:click="getMainList">TEST</button>
-    <ul>
-      <li v-for="l in list" v-bind:key="l">{{l}}</li>
-    </ul>
+    <div class="accountTbList">
+      <colgroup>
+        <col width="10%" />
+        <col width="10%" />
+        <col width="10%" />
+        <col width="10%" />
+        <col width="10%" />
+        <col width="10%" />
+        <col width="10%" />
+        <col width="10%" />
+        <col width="10%" />
+        <col width="10%" />
+      </colgroup>
+      <tr>
+        <th>순번</th>
+        <th>날짜</th>
+        <th>구분</th>
+        <th>주체</th>
+        <th>결제수단</th>
+        <th>대분류</th>
+        <th>소분류</th>
+        <th>금액</th>
+        <th>내용</th>
+        <th>충동구매</th>
+      </tr>
+      <tr v-for="(row, idx) in list" v-bind:key="idx">
+        <td>{{row.account_id}}</td>
+        <td>{{row.account_dt}}</td>
+        <td>{{row.division_nm}}</td>
+        <td>{{row.member_nm}}</td>
+        <td>{{row.payment_nm}}</td>
+        <td>{{row.category_nm}}</td>
+        <td>{{row.category_seq_nm}}</td>
+        <td>{{row.price}}</td>
+        <td>{{row.remark}}</td>
+        <td>{{row.impulse_yn}}</td>
+      </tr>
+    </div>
   </div>
 </template>
 
@@ -20,7 +52,6 @@ export default {
   },
   data() {
       return {
-        name: "서강원",
         list: []
       }
   },
