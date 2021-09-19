9604febddc867321dafe922e6dfc213adb2816ae
tim1047 <tim1047@naevr.com>
2021-09-19 17:45:14 +0900

    comma 필터 추가

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false;
 
+Vue.filter("comma", val =>{
+  return String(val).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+})
+
 new Vue({
   router,
   store,
