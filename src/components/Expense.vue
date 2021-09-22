8b2eee82175f610cc540d17230561c6efde54bb0
tim1047 <tim1047@naevr.com>
2021-09-22 18:48:34 +0900

    가계부 통계 테이블 색상 수정

--- src/components/Expense.vue
+++ src/components/Expense.vue
@@ -1,42 +1,42 @@
 <template>
     <div style="width:45vw; float:left;">
-        <b-table-simple hover small caption-top responsive>
+        <b-table-simple small bordered caption-top responsive>
             <caption>지출:</caption>
             <colgroup><col><col><col></colgroup>
             <colgroup><col><col><col><col><col><col></colgroup>
             <b-thead head-variant="dark">
             <b-tr>
-                <b-th></b-th>
-                <b-th>계획</b-th>
-                <b-th>실제</b-th>
-                <b-th>1W</b-th>
-                <b-th>2W</b-th>
-                <b-th>3W</b-th>
-                <b-th>4W</b-th>
-                <b-th>5W</b-th>
-                <b-th>6W</b-th>
+                <b-th variant="secondary"></b-th>
+                <b-th variant="secondary">계획</b-th>
+                <b-th variant="secondary">실제</b-th>
+                <b-th variant="secondary">1W</b-th>
+                <b-th variant="secondary">2W</b-th>
+                <b-th variant="secondary">3W</b-th>
+                <b-th variant="secondary">4W</b-th>
+                <b-th variant="secondary">5W</b-th>
+                <b-th variant="secondary">6W</b-th>
             </b-tr>
             <b-tr>
-                <b-th>총합</b-th>
-                <b-th>0</b-th>
-                <b-th>{{expenseItems[0].total_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_first_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_second_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_third_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_fourth_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_fifth_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_sixth_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">총합</b-th>
+                <b-th variant="secondary">0</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_first_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_second_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_third_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_fourth_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_fifth_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_sixth_week_sum_price | comma}}</b-th>
             </b-tr>
             </b-thead>
             <b-tbody>
             <b-tr v-for="(item) in expenseItems" v-bind:key="item">
-                <b-th>{{item.category_nm}}</b-th>
-                <b-th>0</b-th>
-                <b-td>{{item.sum_price | comma}}</b-td>
+                <b-th variant="secondary">{{item.category_nm}}</b-th>
+                <b-th variant="secondary">0</b-th>
+                <b-td variant="secondary">{{item.sum_price | comma}}</b-td>
                 <b-td>{{item.first_week_sum_price | comma}}</b-td>
                 <b-td>{{item.second_week_sum_price | comma}}</b-td>
                 <b-td>{{item.third_week_sum_price | comma}}</b-td>
-                <b-td variant="success">{{item.fourth_week_sum_price | comma}}</b-td>
+                <b-td>{{item.fourth_week_sum_price | comma}}</b-td>
                 <b-td>{{item.fifth_week_sum_price | comma}}</b-td>
                 <b-td>{{item.sixth_week_sum_price | comma}}</b-td>
             </b-tr>
--- src/components/ExpenseDtl.vue
+++ src/components/ExpenseDtl.vue
@@ -1,51 +1,51 @@
 <template>
     <div style="width:45vw; float:left;">
-        <b-table-simple hover small caption-top responsive>
+        <b-table-simple small bordered caption-top responsive>
             <caption>지출상세:</caption>
             <colgroup><col><col></colgroup>
             <colgroup><col></colgroup>
             <colgroup><col><col><col><col><col><col></colgroup>
-            <b-thead head-variant="dark">
+            <b-thead head-variant="secondary">
             <b-tr>
-                <b-th colspan="2"></b-th>
-                <b-th>실제</b-th>
-                <b-th>1W</b-th>
-                <b-th>2W</b-th>
-                <b-th>3W</b-th>
-                <b-th>4W</b-th>
-                <b-th>5W</b-th>
-                <b-th>6W</b-th>
+                <b-th colspan="2" variant="secondary"></b-th>
+                <b-th variant="secondary">실제</b-th>
+                <b-th variant="secondary">1W</b-th>
+                <b-th variant="secondary">2W</b-th>
+                <b-th variant="secondary">3W</b-th>
+                <b-th variant="secondary">4W</b-th>
+                <b-th variant="secondary">5W</b-th>
+                <b-th variant="secondary">6W</b-th>
             </b-tr>
             <b-tr>
-                <b-th colspan="2">지출전체총합</b-th>
-                <b-th>{{expenseItems[0].total_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_first_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_second_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_third_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_fourth_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_fifth_week_sum_price | comma}}</b-th>
-                <b-th>{{expenseItems[0].total_sixth_week_sum_price | comma}}</b-th>
+                <b-th colspan="2" variant="secondary">지출전체총합</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_first_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_second_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_third_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_fourth_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_fifth_week_sum_price | comma}}</b-th>
+                <b-th variant="secondary">{{expenseItems[0].total_sixth_week_sum_price | comma}}</b-th>
             </b-tr>
             <b-tr>
-                <b-th colspan="2">고정비용합</b-th>
-                <b-th>0</b-th>
-                <b-th>0</b-th>
-                <b-th>0</b-th>
-                <b-th>0</b-th>
-                <b-th>0</b-th>
-                <b-th>0</b-th>
-                <b-th>0</b-th>
+                <b-th colspan="2" variant="secondary">고정비용합</b-th>
+                <b-th variant="secondary">0</b-th>
+                <b-th variant="secondary">0</b-th>
+                <b-th variant="secondary">0</b-th>
+                <b-th variant="secondary">0</b-th>
+                <b-th variant="secondary">0</b-th>
+                <b-th variant="secondary">0</b-th>
+                <b-th variant="secondary">0</b-th>
             </b-tr>
             </b-thead>
             <b-tbody>
             <b-tr v-for="(item) in expenseItems" v-bind:key="item">
-                <b-th>{{item.category_nm}}</b-th>
-                <b-th>{{item.category_seq_nm}}</b-th>
+                <b-th variant="secondary">{{item.category_nm}}</b-th>
+                <b-th variant="secondary">{{item.category_seq_nm}}</b-th>
                 <b-td>{{item.sum_price | comma}}</b-td>
                 <b-td>{{item.first_week_sum_price | comma}}</b-td>
                 <b-td>{{item.second_week_sum_price | comma}}</b-td>
                 <b-td>{{item.third_week_sum_price | comma}}</b-td>
-                <b-td variant="success">{{item.fourth_week_sum_price | comma}}</b-td>
+                <b-td>{{item.fourth_week_sum_price | comma}}</b-td>
                 <b-td>{{item.fifth_week_sum_price | comma}}</b-td>
                 <b-td>{{item.sixth_week_sum_price | comma}}</b-td>
             </b-tr>
